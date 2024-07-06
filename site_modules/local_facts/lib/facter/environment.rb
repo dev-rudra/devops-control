762d1ffd76396975812156a4996c3a2265c96656
Rudra Kumar Magar <dev.rudrathapamagar@gmail.com>
2024-07-06 21:47:38 +0900

    Add environment facter

---
+++ site_modules/local_facts/lib/facter/environment.rb
@@ -0,0 +1,13 @@
+Facter.add(:environment) do
+    setcode do
+        hostname = Facter.value(:hostname)
+        
+        if hostname.start_with?('junky-')
+            if hostname =~ /^junky-(m|d)/
+                'production'
+            end
+        elsif hostname.start_with?('puppet-')
+            'production'
+        end
+    end
+end
