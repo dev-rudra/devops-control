ed42ff59e27977052ea09b2936f00d39acdc4880
Rudra Kumar Magar <dev.rudrathapamagar@gmail.com>
2024-07-07 20:36:38 +0900

    Add user, vimrc

--- modules/dev_environment/manifests/init.pp
+++ modules/dev_environment/manifests/init.pp
@@ -1,7 +1,24 @@
 class dev_environment {
+  user { 'rudra.magar':
+    ensure      => present,
+    managehome => true,
+    groups       => ['wheel'],
+  }
+
+  package { 'vim':
+    ensure      => present,
+  }
+
+  file { '/home/rudra.magar/.vimrc':
+    ensure      => file,
+    content     => template('dev_environment/vimrc.erb'),
+    owner       => 'rudra.magar',
+    mode        => '0644',
+  }
+
   file { '/usr/local/bin/puppetd':
-    ensure  => file,
-    content => template('dev_environment/puppetd.sh.erb'),
-    mode    => '0755',
+    ensure      => file,
+    content     => template('dev_environment/puppetd.sh.erb'),
+    mode        => '0755',
   }
 }
